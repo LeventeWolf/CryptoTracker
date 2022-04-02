e0b5c4c2f65d38be6501d31139677743fcb367f2
LeventeWolf <wlevi2002@gmail.com>
2022-04-02 17:42:30 +0200

    Init Header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { MainComponent } from './pages/main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
