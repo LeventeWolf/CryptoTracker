ecadfd9a05f1e48dcc3f832ab22c0019817a4cbe
LeventeWolf <wlevi2002@gmail.com>
2022-04-03 19:41:04 +0200

    Update font-style

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-header',
@@ -6,10 +6,18 @@ import {Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @Output() navigateTo: EventEmitter<string> = new EventEmitter();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onLogin() {
+    this.navigateTo.emit('login');
+  }
+
+  onRegister() {
+    this.navigateTo.emit('register');
+  }
 }
