2e013b26f72907926bdda21495651a604dd24c14
LeventeWolf <wlevi2002@gmail.com>
2022-04-04 23:40:10 +0200

    Fetch coins using an API #2

---
+++ src/api/api.ts
@@ -0,0 +1,11 @@
+export const CoinList = (currency: 'usd' | 'huf') =>
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=100&page=1&sparkline=false`;
+
+export const SingleCoin = (id: number) =>
+  `https://api.coingecko.com/api/v3/coins/${id}`;
+
+export const HistoricalChart = (id: number, days = 365, currency: string) =>
+  `https://api.coingecko.com/api/v3/coins/${id}/market_chart?vs_currency=${currency}&days=${days}`;
+
+export const TrendingCoins = (currency: string) =>
+  `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency}&order=gecko_desc&per_page=10&page=1&sparkline=false&price_change_percentage=24h`;
--- src/app/pages/main/crypto-tabel/crypto-tabel.component.ts
+++ src/app/pages/main/crypto-tabel/crypto-tabel.component.ts
@@ -1,4 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Axios from "axios";
+import {CoinList} from '../../../../api/api'
+
+interface Coin  {
+  id: string,
+  name: string,
+  symbol: string,
+  image: string,
+  current_price: number,
+  market_cap: number,
+  price_change_percentage_24h: number;
+}
 
 @Component({
   selector: 'app-crypto-tabel',
@@ -6,10 +18,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./crypto-tabel.component.scss']
 })
 export class CryptoTabelComponent implements OnInit {
+  public coins: Coin[] = [];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  async ngOnInit(): Promise<void> {
+    await fetchCoins('usd').then(coins => {
+      this.coins = coins.slice(0, 10);
+    });
+
+    console.log(this.coins);
   }
 
+  numberWithCommas(num: number) {
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+  }
+
+
 }
+
+async function fetchCoins(currency: 'usd' | 'huf') {
+  let result: any = [];
+
+  await Axios.get(CoinList(currency))
+    .then(response => {
+      result = response.data;
+    })
+    .catch(response => {
+      console.log(`[CryptoTable] FetchCoins Error!`)
+      console.log(response)
+    })
+
+  return result;
+}
+
