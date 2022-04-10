7cee88aa8f3ba0b34c76fb1b8cf89375958e7088
LeventeWolf <wlevi2002@gmail.com>
2022-04-10 16:51:00 +0200

    Share css component between login & register

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -3,13 +3,14 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss', '../login/login.component.scss']
 })
 export class RegisterComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
+
   }
 
 }
