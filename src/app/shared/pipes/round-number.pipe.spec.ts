8299c27681ea82eabbf883a4c310f09c5dd80192
LeventeWolf <wlevi2002@gmail.com>
2022-04-11 00:43:16 +0200

    Remove unused tests

--- src/app/shared/pipes/round-number.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { RoundNumberPipe } from './round-number.pipe';
-
-describe('RoundNumberPipe', () => {
-  it('create an instance', () => {
-    const pipe = new RoundNumberPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
