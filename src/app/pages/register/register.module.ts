43c8906b3d09b7ee5018b2a014b92cc9b63f6d68
LeventeWolf <wlevi2002@gmail.com>
2022-04-10 13:19:10 +0200

    Init register skeleton

---
+++ src/app/pages/register/register.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {RegisterRoutingModule} from "./register-routing.module";
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    RegisterRoutingModule
+  ]
+})
+export class RegisterModule { }
