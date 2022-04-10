149f0b3fd69dd70e95dd4df360ac799bed683e62
LeventeWolf <wlevi2002@gmail.com>
2022-04-11 01:26:27 +0200

    Make crypto-table responsive

--- src/app/pages/main/crypto-table/crypto-table.component.ts
+++ src/app/pages/main/crypto-table/crypto-table.component.ts
@@ -8,6 +8,7 @@ import {Coin, fetchCoins} from '../main.component';
 })
 export class CryptoTableComponent implements OnInit {
   public coins: Coin[] = [];
+  innerWidth = window.innerWidth;
 
   constructor() {}
 
@@ -15,6 +16,19 @@ export class CryptoTableComponent implements OnInit {
     await fetchCoins('usd').then(coins => {
       this.coins = coins.slice(0, 10);
     });
+  }
+
+  onResize($event: any) {
+    // @ts-ignore
+    this.innerWidth = event.target.innerWidth;
+  }
+
+  marketCap(price: number): string {
+    if (this.innerWidth < 600) {
+      return price.toString().substring(0, 3) + ' B';
+    } else {
+      return this.numberWithCommas(price).toString().slice(0, -6) + ' M';
+    }
   }
 
   numberWithCommas(num: number) {
