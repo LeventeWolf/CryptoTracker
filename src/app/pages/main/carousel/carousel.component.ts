e279c661b2ef96871ddb9646cc067659038e75dc
LeventeWolf <wlevi2002@gmail.com>
2022-04-11 08:42:55 +0200

    Make carousel responsive

--- src/app/pages/main/carousel/carousel.component.ts
+++ src/app/pages/main/carousel/carousel.component.ts
@@ -8,23 +8,34 @@ import {Coin, fetchCoins} from "../main.component";
 })
 export class CarouselComponent implements OnInit {
   random_coins: Coin[] = [];
+  innerWidth = window.innerWidth;
+  private readonly _number_of_coins: number = 4;
 
   constructor() {
-
+    if (innerWidth < 600) {
+      this._number_of_coins = 3;
+    } else {
+      this._number_of_coins = 4;
+    }
   }
 
   async ngOnInit(): Promise<void> {
     await fetchCoins('usd').then(coins => {
-      this.random_coins = this.getNRandomCoins(coins, 4);
+      this.random_coins = this.getNRandomCoins(coins, this._number_of_coins);
     });
 
     setInterval(async () => {
       await fetchCoins('usd').then(coins => {
-        this.random_coins = this.getNRandomCoins(coins, 4);
+        this.random_coins = this.getNRandomCoins(coins, this._number_of_coins);
       });
     }, 8000);
 
     // startAnimation();
+  }
+
+  onResize($event: any) {
+    // @ts-ignore
+    this.innerWidth = event.target.innerWidth;
   }
 
 
