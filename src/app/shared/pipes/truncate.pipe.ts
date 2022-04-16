d7865a2097153dfc9e7f49fd3a5dc26fe628a82e
LeventeWolf <wlevi2002@gmail.com>
2022-04-16 13:31:22 +0200

    Create Pipe

---
+++ src/app/shared/pipes/truncate.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'truncate'
+})
+export class TruncatePipe implements PipeTransform {
+
+  transform(value: string | null, args: any[]): string {
+    if (value === null) {
+      return '';
+    }
+
+    const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
+    const trail = args.length > 1 ? args[1] : '...';
+    return value.length > limit ? value.substring(0, limit) + trail : value;
+  }
+
+}
