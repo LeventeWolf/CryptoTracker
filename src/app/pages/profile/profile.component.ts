fc769da92fe88680a6a63835fe7f1eab54b061eb
LeventeWolf <wlevi2002@gmail.com>
2022-04-23 14:16:02 +0200

    Update

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -32,10 +32,3 @@ export class ProfileComponent implements OnInit {
     return firebase.auth().currentUser.uid
   }
 }
-
-
-function transform(value: string, args: any[]): string {
-  const limit = args.length > 0 ? parseInt(args[0], 10) : 20;
-  const trail = args.length > 1 ? args[1] : '...';
-  return value.length > limit ? value.substring(0, limit) + trail : value;
-}
