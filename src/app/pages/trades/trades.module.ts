3f9e1ae30460990553951d841ef695cffb81abcc
LeventeWolf <wlevi2002@gmail.com>
2022-04-24 11:26:00 +0200

    Add <mat-divider>, #17

--- src/app/pages/trades/trades.module.ts
+++ src/app/pages/trades/trades.module.ts
@@ -4,16 +4,20 @@ import { CommonModule } from '@angular/common';
 import { TradesRoutingModule } from './trades-routing.module';
 import { TradesComponent } from './trades.component';
 import {MatTableModule} from "@angular/material/table";
+import {MatDividerModule} from "@angular/material/divider";
+import {MatListModule} from "@angular/material/list";
 
 
 @NgModule({
   declarations: [
     TradesComponent
   ],
-    imports: [
-        CommonModule,
-        TradesRoutingModule,
-        MatTableModule
-    ]
+  imports: [
+    CommonModule,
+    TradesRoutingModule,
+    MatTableModule,
+    MatDividerModule,
+    MatListModule
+  ]
 })
 export class TradesModule { }
