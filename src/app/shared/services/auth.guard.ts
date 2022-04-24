21d79a61a8f40ccd574b5507be9c2fe2c31a1c0d
LeventeWolf <wlevi2002@gmail.com>
2022-04-24 12:36:44 +0200

    Update auth

--- src/app/shared/services/auth.guard.ts
+++ src/app/shared/services/auth.guard.ts
@@ -1,16 +1,28 @@
 import {Injectable} from '@angular/core';
-import {ActivatedRouteSnapshot, CanActivate, RouterStateSnapshot, UrlTree} from '@angular/router';
+import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree} from '@angular/router';
 import {Observable} from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+  constructor(private router: Router) {
+  }
+
   canActivate(
     route: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
 
-    return !!JSON.parse(localStorage.getItem('user') as string);
+    const user = JSON.parse(localStorage.getItem('user') as string);
+
+    if (!user) {
+      this.router.navigateByUrl('')
+        .then(_ => {
+           return false;
+      });
+    }
+
+    return true;
   }
 
 }
